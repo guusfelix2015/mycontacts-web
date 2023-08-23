ca83152b448f233bea17f6fe1007562d7a623773
guusfelix2015 <gusfelix2015@gmail.com>
2023-08-23 15:44:58 -0300

    movendo request pra dentro do component home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,48 @@
-import {Card, Container, Header, InputSearchContainer, ListContainer,} from "./styles";
-import {Link} from "react-router-dom";
+import {
+  Card,
+  Container,
+  Header,
+  InputSearchContainer,
+  ListContainer,
+} from "./styles";
+import { Link } from "react-router-dom";
 import arrow from "../../assets/images/icons/arrow.svg";
 import trash from "../../assets/images/icons/delete.svg";
 import edit from "../../assets/images/icons/edit.svg";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/contacts")
+      .then(async (response) => {
+        const data = await response.json();
+        setContacts(data);
+      })
+      .catch((error) => {
+        console.debug("error", error);
+      });
+  }, []);
+
+  console.log(contacts)
+
   return (
     <Container>
-
       <InputSearchContainer>
-        <input type="text" placeholder="Pesquisar pelo nome.."/>
+        <input type="text" placeholder="Pesquisar pelo nome.." />
       </InputSearchContainer>
       <Header>
         <strong>Contatos</strong>
         <Link to="/new">Novo contato</Link>
       </Header>
-
       <ListContainer>
         <header>
           <button type="button">
             <span>Nome</span>
-            <img src={arrow} alt="Arrow"/>
+            <img src={arrow} alt="Arrow" />
           </button>
         </header>
-
         <Card>
           <div className="info">
             <div className="contact-name">
@@ -35,10 +54,10 @@ export default function Home() {
           </div>
           <div className="actions">
             <Link to="/edit/123">
-              <img src={edit} alt="Edit"/>
+              <img src={edit} alt="Edit" />
             </Link>
             <button>
-              <img src={trash} alt="Trash"/>
+              <img src={trash} alt="Trash" />
             </button>
           </div>
         </Card>
@@ -46,10 +65,3 @@ export default function Home() {
     </Container>
   );
 }
-
-
-fetch("http://localhost:3001/categories").then((response) => {
-  console.log(response)
-}).catch((error) => {
-  console.debug("error", error)
-})
