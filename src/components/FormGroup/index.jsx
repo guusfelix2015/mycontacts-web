7e6e1a576b0b17de6cb73e9e98f5fb571184fc38
guusfelix2015 <gusfelix2015@gmail.com>
2023-09-01 17:37:01 -0300

    aaa

--- src/components/FormGroup/index.jsx
+++ src/components/FormGroup/index.jsx
@@ -1,13 +1,17 @@
+import Spinner from "../Spinner";
 import { Container } from "./styles";
 import PropTypes from "prop-types";
 
 export default function FormGroup({ children, error, isLoading }) {
-  console.log(isLoading);
   return (
     <Container>
       <div className="form-item">
         {children}
-        {isLoading && <div className="loader"></div>}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
 
       {error && <small>{error}</small>}
--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,13 +1,13 @@
 import { Overlay } from "./styles";
 import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
-
+import Spinner from "../Spinner";
 
 export default function Loader({ isLoading }) {
   if (!isLoading) return null;
   return createPortal(
     <Overlay>
-      <div className="loader"></div>
+      <Spinner size={90} />
     </Overlay>,
     document.getElementById("loader-root")
   );
