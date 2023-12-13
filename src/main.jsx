aec341df65ad96eed5778375b6f40d75eb93af7e
guusfelix2015 <gusfelix2015@gmail.com>
2023-12-13 14:36:36 -0300

    criando métodos para adicionar listeners e emitir eventos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 
+import "./lib/EventManager";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
     <App />
